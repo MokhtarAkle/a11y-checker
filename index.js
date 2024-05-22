10ac9716236ea51a2b81fdb9d59ec981a7465044
MokhtarAkle <mokhtar.akle@hva.nl>
2024-05-22 22:35:15 +0200

    :wrench: results mapped to checkpoints and failures

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    aChecker.getCompliance("https://gvb.nl", "GVB").then((results) => {
-        const failed = results.report.results.map((item)=>{
-            if (item.value.includes('FAIL')) {
-                return item
+    aChecker.getCompliance("https://programma.fdnd.nl", "FDND").then((results) => {
+        const failed = [...new Set(results.report.results.map((item) => {
+            if (item.level != 'pass') {
+                return item.ruleId
             }
-        });
-    
+        }))];
+        console.log(failed)
+
         const report = results.report.results.map((item)=>{
                 const {ruleId, value, message, help} = item
                 return { ruleId, value, message, help }
@@ -17,10 +18,29 @@ app.get('/', function (req, res) {
     
         // console.log('POLICIES', results.report.summary.policies)
         //console.log('RAW RESULTS', results.report.results)
-        console.log('REPORT', results.report.summary)
+        console.log('REPORT', results.report)
 
-        aChecker.getRuleset('WCAG_2_2').then(data => {
-            console.log(data.checkpoints[0].rules)
+        aChecker.getRuleset('WCAG_2_1').then(data => {
+            for(let t = 0; t < data.checkpoints.length; t++){
+
+                if(data.checkpoints[t].rules.length > 0){
+                    // console.log(failed);
+
+                    for(let i = 0; i < data.checkpoints[t].rules.length; i++){
+                        // console.log(t, i)
+                            if (failed.includes(data.checkpoints[t].rules[i].id)) {
+
+                                const matchingItem = data.checkpoints[t].rules[i].id + data.checkpoints[t].num;
+                                console.log(matchingItem)
+                            }
+
+                        // console.log(data.checkpoints[t].num,data.checkpoints[t].rules.length);
+
+                    }
+                }
+
+            }
+
         })
     })
     
